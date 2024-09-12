d6c47ba338028ffdde8327a61e39bcbf175bab5f
Dario Fernandez <dariofedes@protonmail.com>
2024-09-12 07:32:42 +0200

    rename home styles

--- src/Home/components/Home.tsx
+++ src/Home/components/Home.tsx
@@ -1,27 +1,26 @@
 import { StyleSheet, View } from 'react-native'
-import useGenres from '../../Genre/useGenres'
-import { Genre } from '../../Genre/types'
-import GenreMovieList from '../../Genre/components/GenreMovieList'
-import spacing from '@styles/spacing'
+import useGenres from '@src/Genre/useGenres'
+import { Genre } from '@src/Genre/types'
+import GenreMovieList from '@src/Genre/components/GenreMovieList'
 import { WishlistMovieList } from '@src/Wishlist/components'
+import spacing from '@styles/spacing'
 
 export default function Home() {
   const { genres } = useGenres()
 
   return (
-    <View style={genreList.wrapper}>
-      {genres && genres.map((genre: Genre) => <GenreMovieList style={genreList.movieList} genre={genre} key={genre.id} />)}
-      <WishlistMovieList />
+    <View style={home.wrapper}>
+      {genres && genres.map((genre: Genre) => <GenreMovieList style={home.movieList} genre={genre} key={genre.id} />)}
+      <WishlistMovieList style={home.movieList} />
     </View>
   )
 }
 
-const genreList = StyleSheet.create({
+const home = StyleSheet.create({
   wrapper: {
     gap: spacing.lg,
   },
 
   movieList: {
-
   },
 })
